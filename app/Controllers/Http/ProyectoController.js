3d0a9b1ca5ebba3ca4acfa79ec14b0f0986104e5
Esteban <esteban.gutm@gmail.com>
2020-03-25 11:58:16 -0300

    mi primer commit

---
+++ app/Controllers/Http/ProyectoController.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const Proyecto = use('App/Models/Proyecto');
+const AutorizacionService = use('App/Services/AutorizacionService');
+
+class ProyectoController {
+    async index({ auth }) {
+        const user = await auth.getUser();
+        // console.log(user.id);
+        return await user.proyectos().fetch();
+    }
+
+    async create({ auth, request}) {
+        const user = await auth.getUser();
+        const { nombre } = request.all();
+        const proyecto = new Proyecto();
+        proyecto.fill({
+            nombre
+        });
+        await user.proyectos().save(proyecto);
+        return proyecto;
+    }
+
+    async destroy({ auth, params}) {
+        const user = await auth.getUser();
+        const { id } = params;
+        // tambien se puee asi, pero es mejor practica la anterior
+        // const id = params.id;
+
+        const proyecto = await Proyecto.find(id);
+
+        AutorizacionService.verificarPermiso(proyecto, user);
+
+        await proyecto.delete();
+        return proyecto;
+
+    }
+
+    async update({ auth, params, request }) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const proyecto = await Proyecto.find(id);
+        AutorizacionService.verificarPermiso(proyecto, user);
+        proyecto.merge(request.only('nombre'));
+        await proyecto.save();
+        return proyecto;
+    }
+
+
+}
+
+module.exports = ProyectoController
